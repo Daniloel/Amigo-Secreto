c7dc99fb311c8abba6d8b304942338964d00902c
Danilo <zoltancao@gmail.com>
2025-02-01 00:51:30 -0300

    Inserida a função para pegar os nomes ao clicar em adicionar

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let entradaListaAmigos = document.querySelector('input').value;
+    amigos.push(entradaListaAmigos);
+    alert(amigos);
+}
+
+
+
+
+
+
